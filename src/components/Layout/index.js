aa5600b0815236fac6adec41f147dacddd7ca038
Henrique <henrique.l.vieira92@gmail.com>
2020-07-29 22:11:36 -0300

    Alteração dos vídeos exibidos na Home

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -10,7 +10,7 @@ const Main = styled.main`
   background-color: var(--black);
   color: var(--white);
   flex: 1;
-  padding-top: 50px;
+  --padding-top: 50px;
   --padding-left: 5%;
   --padding-right: 5%;
 `;
